e84e71076925d8eb6c3dd5020f986275edd94a95
Pascal Schulze <pascal.schulze@student.hpi.uni-potsdam.de>
2021-02-20 19:28:42 +0100

    up

--- flink-connectors/flink-connector-rabbitmq2/src/test/java/benchmarks/Latency.java
+++ flink-connectors/flink-connector-rabbitmq2/src/test/java/benchmarks/Latency.java
@@ -24,7 +24,7 @@ public class Latency {
     String queue = "pub";
     ConsistencyMode mode = ConsistencyMode.AT_MOST_ONCE;
     int n = 5000000;
-    String outputName = "benchmarksEC2/atmostLatencyBenchmark";
+    String outputName = "benchmarksEC2/atmostProcessLatencyBenchmark";
 
     public void sendToRabbit(int n, String queue)
             throws IOException, TimeoutException, InterruptedException {
