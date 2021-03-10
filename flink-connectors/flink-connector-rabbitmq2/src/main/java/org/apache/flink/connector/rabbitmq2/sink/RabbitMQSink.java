9a962a67cd23b1013f8de092f17d66e253ddc404
Yannik Schröder <yannik.schroeder@student.hpi.de>
2021-03-10 11:32:50 +0100

    add: Apache license to all files

--- flink-connectors/flink-connector-rabbitmq2/src/main/java/org/apache/flink/connector/rabbitmq2/sink/RabbitMQSink.java
+++ flink-connectors/flink-connector-rabbitmq2/src/main/java/org/apache/flink/connector/rabbitmq2/sink/RabbitMQSink.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.flink.connector.rabbitmq2.sink;
 
 import org.apache.flink.api.common.serialization.DeserializationSchema;
@@ -30,6 +48,18 @@ import java.util.Optional;
  * It provides at-most-once, at-least-once and exactly-once processing semantics. For at-least-once
  * and exactly-once, checkpointing needs to be enabled. The sink operates as a StreamingSink and
  * thus works in a streaming fashion.
+ *
+ * <pre>{@code
+ * RabbitMQSink
+ *     .builder()
+ *     .setConnectionConfig(connectionConfig)
+ *     .setQueueName("queue")
+ *     .setSerializationSchema(new SimpleStringSchema())
+ *     .setConsistencyMode(ConsistencyMode.AT_LEAST_ONCE)
+ *     .setMinimalResendInterval(10L)
+ *     .build();
+ * }
+ *  * </pre>
  *
  * <p>When creating the sink a {@code connectionConfig} must be specified via {@link
  * RabbitMQConnectionConfig}. It contains required information for the RabbitMQ java client to
--- flink-connectors/flink-connector-rabbitmq2/src/main/java/org/apache/flink/connector/rabbitmq2/sink/RabbitMQSinkBuilder.java
+++ flink-connectors/flink-connector-rabbitmq2/src/main/java/org/apache/flink/connector/rabbitmq2/sink/RabbitMQSinkBuilder.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.flink.connector.rabbitmq2.sink;
 
 import org.apache.flink.api.common.serialization.SerializationSchema;
@@ -11,7 +29,7 @@ import org.apache.flink.connector.rabbitmq2.RabbitMQConnectionConfig;
  * minimalResendIntervalMilliseconds} (for at-least-once), {@code maxRetry} threshold for resending
  * behaviour and a {@code returnListener}.
  *
- * <pre>
+ * <pre>{@code
  *  RabbitMQSink
  *    .builder()
  *    .setConnectionConfig(connectionConfig)
@@ -20,6 +38,7 @@ import org.apache.flink.connector.rabbitmq2.RabbitMQConnectionConfig;
  *    .setConsistencyMode(ConsistencyMode.AT_LEAST_ONCE)
  *    .setMinimalResendInterval(10L)
  *    .build();
+ * }
  * </pre>
  */
 public class RabbitMQSinkBuilder<T> {
